b90c6a477dd03987194274a1b87a792ec2f939ce
malyda <malyce.dvd@gmail.com>
2019-03-10 12:33:04 +0100

    Init

---
+++ assets/js/vendor/map.js
@@ -0,0 +1,45 @@
+        /*google map*/
+        // When the window has finished loading create our google map below
+        google.maps.event.addDomListener(window, 'load', init);
+
+        function init() {
+            // Basic options for a simple Google Map
+            // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+            var mapOptions = {
+                // How zoomed in you want the map to start at (always required)
+                zoom: 17,
+
+                scrollwheel: false,
+
+                // The latitude and longitude to center the map (always required)
+                center: new google.maps.LatLng(50.0481404, 14.4563637), // New York
+
+                // How you would like to style the map.
+                // This is where you would paste any style found on Snazzy Maps.
+                styles: [{
+                    "featureType": "all",
+                    "elementType": "all",
+                    "stylers": [{
+                            "saturation": 0
+                        },
+                        {
+                            "gamma": 0.5
+                        }
+                    ]
+                }]
+            };
+
+            // Get the HTML DOM element that will contain your map
+            // We are using a div with id="map" seen below in the <body>
+            var mapElement = document.getElementById('googlemap');
+
+            // Create the Google Map using our element and options defined above
+            var map = new google.maps.Map(mapElement, mapOptions);
+
+            // Let's also add a marker while we're at it
+            var marker = new google.maps.Marker({
+                position: new google.maps.LatLng(50.0481404, 14.4563637),
+                map: map,
+                title: 'DND Conf'
+            });
+        }
